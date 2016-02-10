40f35a9a3e65b3f94f537268a451d8c4c17bae5a
Clayton Hynfield <clayton@hynfield.org>
2016-02-10 01:25:27 -0500

    Add test for `pip list` command

--- spec/default/sample_spec.rb
+++ spec/default/sample_spec.rb
@@ -3,3 +3,7 @@ require 'spec_helper'
 describe command('defaults read'), :if => os[:family] == 'darwin' do
   its(:exit_status) { should eq 0 }
 end
+
+describe command('pip list') do
+  its(:exit_status) { should eq 0 }
+end
