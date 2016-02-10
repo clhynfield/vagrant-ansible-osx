6bc665a42711ebe1120172fc98e8354696a39896
Clayton Hynfield <clayton@hynfield.org>
2016-02-10 01:21:49 -0500

    Replace tests with super-simple OS X test

--- spec/default/sample_spec.rb
+++ spec/default/sample_spec.rb
@@ -1,13 +1,5 @@
 require 'spec_helper'
 
-describe package('pv'), :if => os[:family] == 'darwin' do
-  it { should be_installed }
-end
-
-describe port(88) do
-  it { should be_listening }
-end
-
-describe port(80) do
-  it { should_not be_listening }
+describe command('defaults read'), :if => os[:family] == 'darwin' do
+  its(:exit_status) { should eq 0 }
 end
